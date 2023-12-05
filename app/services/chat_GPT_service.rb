747906452d88edfee810b9702628fb7990b14398
Daniel Gordon <d_gordon1@icloud.com>
2023-12-05 17:06:05 +0100

    rubocop

--- app/services/chat_GPT_service.rb
+++ app/services/chat_GPT_service.rb
@@ -7,20 +7,13 @@ class ChatGptService
   API_KEY = ENV['CHAT_GPT'].freeze
 
   def self.get_new_recipe_suggestions(recipe_titles:, ingredients:)
-    request_payload = {
-      favorite_recipes:,
-      ingredients:
-    }
+    request_payload = { favorite_recipes:, ingredients: }
     begin
-      response = RestClient.post(
-        CHATGPT_API_ENDPOINT,
-        request_payload.to_json,
-        { Authorization: "Bearer #{API_KEY}", 'Content-Type': 'application/json' }
-      )
+      response = RestClient.post(CHATGPT_API_ENDPOINT, request_payload.to_json,
+                                 { Authorization: "Bearer #{API_KEY}", 'Content-Type': 'application/json' })
       parsed_response = JSON.parse(response.body)
       parsed_response['new_recipe_suggestions'].map { |recipe_json| Recipe.new(title: recipe_json['title']) }
     rescue RestClient::ExceptionWithResponse => e
-      # Handle API request errors
       Rails.logger.debug { "Error: #{e.response}" }
       nil
     end
