78539af6d867ac0c0a8ccec8c2d7dcfd79f2ecd7
Daniel Gordon <d_gordon1@icloud.com>
2023-12-05 17:03:26 +0100

    some refactor work\

--- app/services/recipe_parser.rb
+++ app/services/recipe_parser.rb
@@ -5,10 +5,7 @@ class RecipeParser
     @file_path = file_path
     extension = file_extension(file_path)
 
-    unless SUPPORTED_FORMATS.include?(extension)
-      raise ArgumentError,
-            "Unsupported file format: .#{extension}. Only #{SUPPORTED_FORMATS.join(', ')} files are supported."
-    end
+    validate_extension(extension)
 
     case extension
     when 'json'
@@ -25,8 +22,8 @@ class RecipeParser
   def parse_and_insert_efficiently
     recipes = JSON.parse(@json_data)
     # Preload data
-    categories = RecipeCategory.all.to_a
-    authors = User.all.to_a
+    @categories = RecipeCategory.all.to_a
+    @authors = User.all.to_a
 
     # Batch Process data
     batch_size = 100 # Adjust the batch size based on the dataset and system capacity
@@ -36,16 +33,7 @@ class RecipeParser
       recipe_batch.map! do |recipe_data|
         # Store ingredients in an array
         recipe_ingredients << recipe_data['ingredients'].map { |ingredient| { description: ingredient } }
-        {
-          title: recipe_data['title'],
-          cooking_time_minutes: recipe_data['cook_time'],
-          preparation_time_minutes: recipe_data['prep_time'],
-          rating: recipe_data['ratings'],
-          cuisine: recipe_data['cuisine'],
-          recipe_category_id: categories.find { |category| category.name == recipe_data['category'] }&.id,
-          user_id: authors.find { |author| author.username == recipe_data['author'] }&.id, 
-          image_url: recipe_data['image']
-        }
+        build_recipe(recipe_data)
       end
       # Insert and upsert skip AR validations and callbacks.
       # Luckily we have some DB level constraints.
@@ -61,5 +49,26 @@ class RecipeParser
       end
       Ingredient.upsert_all(ingredients)
     end
+  end
+
+  private
+
+  def build_recipe(recipe_data)
+    {
+      title: recipe_data['title'],
+      cooking_time_minutes: recipe_data['cook_time'],
+      preparation_time_minutes: recipe_data['prep_time'],
+      rating: recipe_data['ratings'],
+      cuisine: recipe_data['cuisine'],
+      recipe_category_id: @categories.find { |category| category.name == recipe_data['category'] }&.id,
+      user_id: @authors.find { |author| author.username == recipe_data['author'] }&.id,
+      image_url: recipe_data['image']
+    }
+  end
+
+  def validate_extension(extension)
+    return if SUPPORTED_FORMATS.include?(extension)
+
+    raise ArgumentError, "Unsupported file format: .#{extension}. Only #{SUPPORTED_FORMATS.join(', ')} files are supported."
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@
 FactoryBot.define do
   factory :user do
     username { "author_#{rand(1...100)}" }
-    email { "author_#{rand(1...100)}@email.com" }
+    email { "author_#{rand(1...10_000)}@email.com" }
     password { SecureRandom.hex(10) }
   end
 end
