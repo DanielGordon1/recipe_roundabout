235caef24145494799d5f572ad89882a1c2fec2c
Daniel Gordon <d_gordon1@icloud.com>
2023-12-12 22:21:03 +0100

    fix deploy

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -1,1 +1,7 @@
-Flipper.enable_percentage_of_actors(:chat_gpt_recommendations, ENV['GPT_RECIPE_PERCENTAGE'])
+# Need this workaround to make sure deploying to fly does not break because it has no DB during the build process.
+begin
+  ActiveRecord::Base.connection
+rescue ActiveRecord::ConnectionNotEstablished
+else
+  Flipper.enable_percentage_of_actors(:chat_gpt_recommendations, ENV['GPT_RECIPE_PERCENTAGE'])
+end
