22930862662c9197a40b0d6b4a43e898b316cc1c
Daniel Gordon <d_gordon1@icloud.com>
2023-12-13 12:17:55 +0100

    Manually add flipper init in test environment

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -2,9 +2,8 @@
 # We need this workaround to make sure that whenever we run a rails/rake command the process does not break.
 # F.E. when deploying to fly.io there is no DB in the build process.
 Rails.application.config.after_initialize do
+  # Only load this config when a server is started or when the rails console is started.
   if defined?(::Rails::Server) || defined?(::Rails::Console)
     Flipper.enable_percentage_of_actors(:chat_gpt_recommendations, ENV['GPT_RECIPE_PERCENTAGE'])
-      # Rails.logger.info("Doing some init")
-      # ...
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,6 +29,7 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
+Flipper.enable_percentage_of_actors(:chat_gpt_recommendations, ENV.fetch('GPT_RECIPE_PERCENTAGE'))
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
@@ -41,6 +42,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
