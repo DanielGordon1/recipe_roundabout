c029e21b073573e906b20a7f6ab2e1956d9af741
Daniel Gordon <d_gordon1@icloud.com>
2023-12-13 12:13:38 +0100

    adjust workaround

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -1,10 +1,10 @@
 # Initializers are loaded each time a rails command is fired.
 # We need this workaround to make sure that whenever we run a rails/rake command the process does not break.
 # F.E. when deploying to fly.io there is no DB in the build process.
-# Also when running rails db:create we first load the initializers, then the command is executed.
-begin
-  ActiveRecord::Base.connection
-rescue ActiveRecord::ConnectionNotEstablished, ActiveRecord::NoDatabaseError
-else
-  Flipper.enable_percentage_of_actors(:chat_gpt_recommendations, ENV['GPT_RECIPE_PERCENTAGE'])
+Rails.application.config.after_initialize do
+  if defined?(::Rails::Server) || defined?(::Rails::Console)
+    Flipper.enable_percentage_of_actors(:chat_gpt_recommendations, ENV['GPT_RECIPE_PERCENTAGE'])
+      # Rails.logger.info("Doing some init")
+      # ...
+  end
 end
