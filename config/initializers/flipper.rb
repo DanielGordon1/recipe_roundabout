3e614392c78c674bc29a409256003549dcf23ab1
Daniel Gordon <d_gordon1@icloud.com>
2023-12-13 11:54:15 +0100

    extend comments

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -1,7 +1,10 @@
-# Need this workaround to make sure deploying to fly.io does not break due to it having no DB in the build process.
+# Initializers are loaded each time a rails command is fired.
+# We need this workaround to make sure that whenever we run a rails/rake command the process does not break.
+# F.E. when deploying to fly.io there is no DB in the build process.
+# Also when running rails db:create we first load the initializers, then the command is executed.
 begin
   ActiveRecord::Base.connection
-rescue ActiveRecord::ConnectionNotEstablished
+rescue ActiveRecord::ConnectionNotEstablished, ActiveRecord::NoDatabaseError
 else
   Flipper.enable_percentage_of_actors(:chat_gpt_recommendations, ENV['GPT_RECIPE_PERCENTAGE'])
 end
